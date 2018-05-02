9b99a8eb7daf7031b336b714337fde88721387ca
Alexander Strole <astrole@student.42.us.org>
2018-05-02 01:16:56 -0700

    first

--- source/app.d
+++ source/app.d
@@ -6,6 +6,6 @@ import game;
 
 void main()
 {
-	auto game = new Game(409, 409, Color(255, 255, 255));
+	auto game = new Game(250, 250, Color(255, 255, 255));
 	game.run();
 }
--- source/game.d
+++ source/game.d
@@ -13,9 +13,8 @@ import helpFuncs;
 
 struct Cell
 {
-	Vector2f pos;
-	ubyte a;
-	ubyte b;
+	float a;
+	float b;
 }
 
 class Game
@@ -29,15 +28,16 @@ class Game
 	Vertex[][] positions;
 	float da = 1.0;
 	float db = 0.5;
-	float f = 0.055;
+	float f = 0.0545;
 	float k = 0.062;
+	float t = 1.0;
 
 	this(int x, int y, Color color)
 	{
 		this.size = Vector2i(x, y);
 		this.color = color;
 		this.window = new RenderWindow(VideoMode(this.size.x, this.size.y), "Reaction Diffusion Algorithm");
-		this.window.setFramerateLimit(60);
+		this.window.setFramerateLimit(120);
 
 		this.cells.length = this.size.x;
 		this.next.length = this.size.x;
@@ -49,11 +49,14 @@ class Game
 			this.positions[i].length = this.size.y;
 			foreach (j; 0 .. this.size.y)
 			{
-				this.cells[i][j] = new Cell(Vector2f(i, j), 0, 1);
-				this.next[i][j] = new Cell(Vector2f(i, j), 0, 0);
+				this.cells[i][j] = new Cell(1.0, 0.0);
+				this.next[i][j] = new Cell(1.0, 0.0);
 				this.positions[i][j] = Vertex(Vector2f(i, j));
 			}
 		}
+		foreach (i; (this.size.x / 2) - 10 .. (this.size.x / 2) + 10)
+			foreach (j; (this.size.y / 2) - 10 .. (this.size.y / 2) + 10)
+				this.cells[i][j].b = 1.0;
 	}
 
 	void run()
@@ -68,25 +71,28 @@ class Game
 				{
 					float a = this.cells[i][j].a;
 					float b = this.cells[i][j].b;
-					float apart = a +
-							      (this.da * this.laplaceA(i, j)) - 
-							  	  (a * b * b) + 
-							  	  (this.f * (1 - a));
-					float bpart = b + 
-							  	  (this.db * this.laplaceB(i, j)) +
-							  	  (a * b * b) -
-							  	  ((this.k + this.f) * b);
-					apart = constrain(apart * 255, 0, 255);
-					bpart = constrain(bpart * 255, 0, 255);
-					this.next[i][j].a = to!ubyte(apart);
-					this.next[i][j].b = to!ubyte(bpart);	
+					this.next[i][j].a = a +
+							      		(this.da * this.laplaceA(i, j)) - 
+							  	  		(a * b * b) + 
+							  	  		(this.f * (1 - a)) * this.t;
+					this.next[i][j].b = b + 
+							  	  		(this.db * this.laplaceB(i, j)) +
+							  	  		(a * b * b) -
+							  	  		((this.k + this.f) * b) * this.t;
+					this.next[i][j].a = constrain(this.next[i][j].a, 0.0, 1.0);
+					this.next[i][j].b = constrain(this.next[i][j].b, 0.0, 1.0);
 				}
 			}
 			foreach (i; 0 .. this.size.x)
 			{
 				foreach (j; 0 .. this.size.y)
 				{
-					this.positions[i][j].color = Color(this.next[i][j].a, 0, this.next[i][j].b);
+					float a = this.next[i][j].a;
+					float b = this.next[i][j].b;
+					ubyte au = to!ubyte(constrain(a * 255, 0, 255));
+					ubyte bu = to!ubyte(constrain(b * 255, 0, 255));
+					ubyte color = to!ubyte(constrain((a - b) * 255, 0, 255));
+					this.positions[i][j].color = Color(bu, color, au, 255);
 				}
 			}
 			foreach (i; 0 .. this.positions.length)
@@ -121,7 +127,7 @@ class Game
 		sumA += this.cells[i - 1][j].a * 0.2;
 		sumA += this.cells[i + 1][j].a * 0.2;
 		sumA += this.cells[i][j + 1].a * 0.2;
-		sumA += this.cells[i][j + 1].a * 0.2;
+		sumA += this.cells[i][j - 1].a * 0.2;
 		sumA += this.cells[i - 1][j - 1].a * 0.05;
 		sumA += this.cells[i + 1][j - 1].a * 0.05;
 		sumA += this.cells[i + 1][j + 1].a * 0.05;
@@ -138,7 +144,7 @@ class Game
 		sumB += this.cells[i - 1][j].b * 0.2;
 		sumB += this.cells[i + 1][j].b * 0.2;
 		sumB += this.cells[i][j + 1].b * 0.2;
-		sumB += this.cells[i][j + 1].b * 0.2;
+		sumB += this.cells[i][j - 1].b * 0.2;
 		sumB += this.cells[i - 1][j - 1].b * 0.05;
 		sumB += this.cells[i + 1][j - 1].b * 0.05;
 		sumB += this.cells[i + 1][j + 1].b * 0.05;
